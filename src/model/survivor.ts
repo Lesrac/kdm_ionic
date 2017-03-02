e7646908cd28ec489f72c9dd43b2dd7e9e17c221
Lesrac <daniel.frick88@gmail.com>
2017-03-02 10:57:45 +0100

    added some points to survivor page

--- src/model/survivor.ts
+++ src/model/survivor.ts
@@ -6,6 +6,38 @@ export class Survivor {
   id: number;
   name: string;
   isAlive: boolean = true;
+  isMale: boolean = true;
+  experience: number = 0;
+  survival: number = 0;
+  canDodge: boolean = true;
+  canEncourage: boolean = false;
+  canSurge: boolean = false;
+  canDash: boolean = false;
+  movement: number = 5;
+  accuracy: number = 0;
+  strength: number = 0;
+  evasion: number = 0;
+  luck: number = 0;
+  speed: number = 0;
+  insanity: number = 0;
+  brainDamage: boolean = false;
+  headArmor: number = 0;
+  headHeavyInjury: boolean = false;
+  armsArmor: number = 0;
+  armsLightInjury: boolean = false;
+  armsHeavyInjury: boolean = false;
+  bodyArmor: number = 0;
+  bodyLightInjury: boolean = false;
+  bodyHeavyInjury: boolean = false;
+  waistArmor: number = 0;
+  waistLightInjury: boolean = false;
+  waistHeavyInjury: boolean = false;
+  legsArmor: number = 0;
+  legsLightInjury: boolean = false;
+  legsHeavyInjury: boolean = false;
+  cannotUseFightingArts: boolean = false;
+  cannotSpendSurvival: boolean = false;
+  skipNextHunt: boolean = false;
 
   constructor(name: string) {
     this.name = name;
--- src/pages/survivor/survivor.ts
+++ src/pages/survivor/survivor.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Survivor } from '../../model/survivor';
 import { NavParams, ModalController, NavController } from 'ionic-angular';
+import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
 /**
  * Created by Daniel on 01.03.2017.
  */
@@ -10,13 +11,47 @@ import { NavParams, ModalController, NavController } from 'ionic-angular';
 })
 export class SurvivorPage implements OnInit {
 
+  private static MAX_XP: number = 16;
+
   survivor: Survivor;
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public params: NavParams) {
+  xpGroup: FormGroup;
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public params: NavParams,
+              public formBuilder: FormBuilder) {
     this.survivor = params.get('survivor');
   }
 
   ngOnInit(): void {
+    this.setupXP();
+  }
+
+  increaseSurvival(): void {
+    this.survivor.survival++;
+  }
+
+  decreaseSurvival(): void {
+    this.survivor.survival--;
+  }
+
+  updateXP(event: Event, control: FormControl): void {
+    if (control.value) {
+      this.survivor.experience++;
+    } else {
+      this.survivor.experience--;
+    }
+  }
+
+  private setupXP(): void {
+    const checkboxArray = new FormArray([]);
+    for (let i: number = 0; i < SurvivorPage.MAX_XP; i++) {
+      if (i < this.survivor.experience) {
+        checkboxArray.push(new FormControl(true));
+      } else {
+        checkboxArray.push(new FormControl(false));
+      }
+    }
+    this.xpGroup = this.formBuilder.group({xps: checkboxArray});
   }
 
 }
