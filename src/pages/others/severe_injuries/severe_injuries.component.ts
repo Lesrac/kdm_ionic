67d1b7d411b7a6accca8940611d7fe83dbbb7a39
Lesrac <daniel.frick88@gmail.com>
2017-10-22 15:18:19 +0200

    added all glossary entries

--- src/pages/others/severe_injuries/severe_injuries.component.ts
+++ src/pages/others/severe_injuries/severe_injuries.component.ts
@@ -11,9 +11,11 @@ import { SevereInjuriesDetailPageComponent } from './severe_injuries_detail.comp
   templateUrl: 'severe_injuries.component.html',
 })
 export class SevereInjuriesPageComponent {
-  text1: string = 'When a survivor suffers a severe injury at a hit location, determine the outcome by rolling 1d10 on the corresponding table.';
+  text1: string = 'When a survivor suffers a severe injury at a hit location, determine the outcome by rolling 1d10' +
+    ' on the corresponding table.';
   text2: string = 'A survivor with 5 bleeding tokens is dead.';
-  text3: string = 'Some permanent injuries have limits to how many times they can be recorded. If a survivor suffers a severe injury that they have already recorded the maximum number of times, they instead gain 1 bleeding token.';
+  text3: string = 'Some permanent injuries have limits to how many times they can be recorded. If a survivor suffers' +
+    ' a severe injury that they have already recorded the maximum number of times, they instead gain 1 bleeding token.';
 
   constructor(public navCtrl: NavController) {
 
