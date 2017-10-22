499841bc91cebc405293be466926039eba3e98fe
Lesrac <daniel.frick88@gmail.com>
2017-10-22 20:33:34 +0200

    huntEvents: search result list

--- src/pages/others/hunt_event_table/hunt_event_table.component.ts
+++ src/pages/others/hunt_event_table/hunt_event_table.component.ts
@@ -12,13 +12,28 @@ import { HuntEvent } from '../../../model/hunte_event';
   templateUrl: 'hunt_event_table.component.html',
 })
 export class HuntEventTablePageComponent implements OnInit {
-  huntEvents: HuntEvent[];
+  filteredHuntEvents: HuntEvent[];
+  allHuntEvents: HuntEvent[];
 
   constructor(public navCtrl: NavController, private kdmService: KDMDataService) {
   }
 
   ngOnInit(): void {
-    this.kdmService.getAllHuntEvents().then(huntEvents => this.huntEvents = huntEvents);
+    this.kdmService.getAllHuntEvents().then(huntEvents => {
+      this.allHuntEvents = huntEvents;
+      this.filteredHuntEvents = huntEvents;
+    });
+  }
+
+  filterEventTable(event: any) {
+    let val = event.target.value;
+    if (val && val.trim() !== '') {
+      this.filteredHuntEvents = this.allHuntEvents.filter(huntEvent =>
+        huntEvent.name.toLowerCase().includes(val.toLowerCase().trim()) ||
+        huntEvent.rollResult === +val);
+    } else {
+      this.filteredHuntEvents = this.allHuntEvents;
+    }
   }
 
 }
