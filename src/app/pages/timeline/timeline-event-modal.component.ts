fc06d3d1e587d46c7a236f37d5f6a9499ddff1fd
Lesrac <daniel.frick88@gmail.com>
2018-11-30 22:57:11 +0100

    fix: corrected input()

--- src/app/pages/timeline/timeline-event-modal.component.ts
+++ src/app/pages/timeline/timeline-event-modal.component.ts
@@ -5,9 +5,10 @@ import { LanternEvent } from '../../model/lantern-event';
  * Created by Daniel on 04.02.2017.
  */
 @Component({
-  selector: 'kdmf-timeline-event-modal', templateUrl: 'timeline-event-modal.component.html',
+    selector: 'kdmf-timeline-event-modal', templateUrl: 'timeline-event-modal.component.html',
 })
 export class TimelineEventModalComponent {
-  // TODO get the value in this component
-  @Input lanternEvent: LanternEvent;
+    // TODO get the value in this component
+    @Input()
+    lanternEvent: LanternEvent;
 }
