e3232836ae820367fae6f1738ee04e9e2bc550f5
Lesrac <daniel.frick88@gmail.com>
2018-12-03 20:31:21 +0100

    fix: typo in route to others

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       },
       {
         path: 'other',
-        loadChildren: '.pages/others/others.module#OthersPageModule',
+        loadChildren: './pages/others/others.module#OthersPageModule',
       },
       {
         path: 'survivors',
